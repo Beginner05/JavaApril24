7fca4d1cd8c8e1d05bd0a39d8013e0a5b8bcfef4
Hitesh <84154077+Beginner05@users.noreply.github.com>
2024-07-23 22:57:25 +0530

    Add files via upload

--- Subset.java
+++ Subset.java
@@ -1,21 +1,40 @@
-package Recursion1;
+package Recursion;
 
+import java.util.Scanner;
 public class Subset {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int arr[]= {1,2,3};
-		sol(arr,0,"");
+		// TODO Auto-genrated method stub
+Scanner scn=new Scanner(System.in);
+int n=scn.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<arr.length;i++)
+{
+	arr[i]=scn.nextInt();
+}
+sol(arr);
 	}
-	public static void sol(int arr[],int idx,String ans)
+	public static void sol(int arr[])
 	{
-		if(idx==arr.length)
+		int total=(int)Math.pow(2, arr.length);
+		for(int i=0;i<total;i++)
 		{
-			System.out.println(ans);
-			return;
+			String str=Integer.toBinaryString(i);
+//			System.out.println(str);
+			String ans="";
+			int n=i;
+for(int j=arr.length-1;j>=0;j--)
+{
+int rem=n%2;
+if(rem==1)
+{
+	ans=arr[j]+ans;
+	
+}
+n=n/2;
+}
+System.out.println(ans);
 		}
-		sol(arr,idx+1,ans);
-		sol(arr,idx+1,ans+arr[idx]);
 		
 	}
 
