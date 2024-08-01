e0d10c39d330b4463496e2843466fe1bcc2d4b82
Hitesh <84154077+Beginner05@users.noreply.github.com>
2024-08-01 21:28:19 +0530

    Update lexicalorder.java

--- lexicalorder.java
+++ lexicalorder.java
@@ -5,6 +5,33 @@ public class lexicalorder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ArrayList<Integer>list=new ArrayList();
+sol(13,list,0);
 	}
-
+	  public static void sol(int n,ArrayList<Integer>list,int  ans)
+	    {
+	        
+	         if(ans<=n&&ans!=0)
+	        {
+	        
+	             list.add(ans);
+	        }
+	        
+	        if(ans>n)return;
+	       for(int i=0;i<=9;i++)
+	        {
+	           if(i==0&&ans==0)
+	           {
+	               continue;
+	           }
+	           if(ans==0)
+	           {
+	            sol(n,list,i);   
+	           }
+	           else{
+	               sol(n,list,(ans*10)+i);
+	           }
+	            
+	        }
+	    }
 }
